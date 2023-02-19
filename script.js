bd482f3dcd61534be4d779f166d65b983073c5c9
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2023-02-19 17:01:41 +1300

    Add playRound fn with logic to determine round winner

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-window.addEventListener('load', function(){
-    
+window.addEventListener('load', function () {
     // getComputerChoice()
     function getComputerChoice() {
-        const choices = ['rock', 'paper', 'scissors']
-        const randomChoice = Math.floor(Math.random() * choices.length)
-        const computerChoice = choices[randomChoice]
+        const choices = ['rock', 'paper', 'scissors'];
+        const randomChoice = Math.floor(Math.random() * choices.length);
+        const computerChoice = choices[randomChoice];
 
         return computerChoice;
     }
@@ -18,6 +17,26 @@ window.addEventListener('load', function(){
         // If playerSelection is paper and computerSelection is scissors, computer wins, else player wins
         // If playerSelection is scissors and computerSelection is rock, computer wins, else player wins
 
+        if (playerSelection.toLowerCase() === computerSelection) {
+            console.log(`It's a tie`);
+        } else if (
+            playerSelection.toLowerCase() === 'rock' &&
+            computerSelection === 'paper'
+        ) {
+            console.log('Computer wins');
+        } else if (
+            playerSelection.toLowerCase() === 'paper' &&
+            computerSelection === 'scissors'
+        ) {
+            console.log('Computer wins');
+        } else if (
+            playerSelection.toLowerCase() === 'scissors' &&
+            computerSelection === 'rock'
+        ) {
+            console.log('Computer wins');
+        } else {
+            console.log(`Player wins`);
+        }
     }
 
     // game()
@@ -27,7 +46,7 @@ window.addEventListener('load', function(){
         // Keep track of player and computer scores
         // Determine winner at the end of five rounds
         for (let round = 0; round < 5; round++) {
-            playRound(playerSelection, computerSelection)
+            playRound(playerSelection, computerSelection);
         }
     }
-})
+});
